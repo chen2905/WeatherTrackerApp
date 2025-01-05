509e0232ee04cad38ddd4d3426464a4e941b3dc0
ChenGao <maxlin2905@gmail.com>
2025-01-06 10:28:12 +1100

    application insight

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    openWeatherMapApiKey: 'd6feb9c16098c3ff8559c13df41ed855'  // Replace with your actual OpenWeatherMap API key
+    openWeatherMapApiKey: 'd6feb9c16098c3ff8559c13df41ed855',  // Replace with your actual OpenWeatherMap API key,
+    insightInstrumentationKey: '94fc47ff-57c1-4745-b3fe-26932cd14f6c'
   };
   
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,21 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';  // Import HttpClientModule
 import { provideHttpClient } from '@angular/common/http';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { environment } from './environments/environment';
+
+
+const appInsights = new ApplicationInsights({
+  config:{
+    instrumentationKey:environment.insightInstrumentationKey
+  },
+
+});
+
+appInsights.loadAppInsights();
+appInsights.trackPageView();
+
+
 
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient()]  // Provide HttpClient globally
